157bf5b44d7c605cae76106744a576e2e614c737
chendou <chenchendoudou@gmail.com>
2026-01-07 08:14:47 +0800

    调整注释

--- code/ecf/signal1.c
+++ code/ecf/signal1.c
@@ -2,7 +2,7 @@
 /* $begin signal1 */
 /* WARNING: This code is buggy! */
 
-void handler1(int sig) 
+void handler1(int sig)
 {
     int olderrno = errno;
 
@@ -13,7 +13,7 @@ void handler1(int sig)
     errno = olderrno;
 }
 
-int main() 
+int main()
 {
     int i, n;
     char buf[MAXBUF];
@@ -22,21 +22,28 @@ int main()
         unix_error("signal error");
 
     /* Parent creates children */
-    for (i = 0; i < 3; i++) {
-        if (Fork() == 0) {
+    for (i = 0; i < 3; i++)
+    {
+        if (Fork() == 0)
+        {
             printf("Hello from child %d\n", (int)getpid());
             exit(0);
         }
     }
 
+    //STDIN_FILENO
+    //标准输入的文件描述符（值为 0）
+    //等同于 stdin的底层描述符
     /* Parent waits for terminal input and then processes it */
     if ((n = read(STDIN_FILENO, buf, sizeof(buf))) < 0)
         unix_error("read");
 
     printf("Parent processing input\n");
     while (1)
-        ;
-
+    {
+        sleep(1);
+        printf("循环\n");
+    }
     exit(0);
 }
 /* $end signal1 */
